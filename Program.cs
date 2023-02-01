d71ceab869257d56fb72b509f8e30be1b12ee03b
Davar Archibald <44330028+MasterAnalysis@users.noreply.github.com>
2023-02-01 13:23:02 -0500

    Program src

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace python
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PythonOp pop = new PythonOp();
+            pop.Initialize();
+            pop.Execute();
+        }
+    }
+}
---
+++ PythonOp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Python.Runtime;
+
+namespace challenge
+{
+    public class PythonOp
+    {
+       using (PyModule scope = Py.CreateScope())
+
+        public PythonOp() { 
+
+        }
+        public void Initialize() {
+
+            try {
+                PythonEngine.Initialize();
+                PythonEngine.BeginAllowThreads()
+                scope = Py.CreateScope();
+            } 
+            catch { 
+      
+
+            }
+        }
+        public void Execute()
+        {
+            using (Py.GIL())
+            {
+                scope.Exec();
+            }
+        }
+    }
+}
